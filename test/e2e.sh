ce729935a63e478322e746f9bbaeb35203a7e5ec
mcnichol <mcnichol.m@gmail.com>
2020-04-16 13:18:21 -0500

    Clean bin and tmp folders for e2e test

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=${BASH_SOURCE%/*}
+
+echo "Deleting downloaded binaries"
+rm $SCRIPT_DIR/../bin/pb
+rm $SCRIPT_DIR/../bin/duffle
+rm $SCRIPT_DIR/../tmp/build-service-0.1.0.tgz
+
 ./pbsetup
 ./pbsetup create gke-cluster
 ./pbsetup delete gke-cluster
